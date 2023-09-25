1df253d18c17e56055fddb5afb782a0096a3bc50
tangxiaoxin <tangxiaoxin@kuaishou.com>
2023-09-25 14:10:25 +0800

    ts

--- src/polify.ts
+++ src/polify.ts
@@ -1,8 +1,10 @@
 const polyfill = () => {
   if (!String.prototype.matchAll) {
+    //@ts-ignore
     String.prototype.matchAll = function (regexp: RegExp) {
       const matches = [];
       let match: any;
+      //@ts-ignore
       while ((match = regexp.exec(this)) != null) {
         matches.push(match);
       }
